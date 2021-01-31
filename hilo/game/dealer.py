9863c8a8c97d6fb54956409b7008b963a69c301f
Benjamin Davis <ben.yj.davis@gmail.com>
2021-01-31 00:44:58 -0500

    Chooses card from Cards and shuffles deck before returning chosen card to Director.

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,11 +1,13 @@
-from hilo.game.cards import Cards
+from game import Cards
 from hilo.game.director import Director
 import random
 
 class Dealer:
     def __init__(self):
         self.cards = Cards()
+        lastChoice = 0
         
     def dealCards(self):
-        randCard = random.randint(0, 52)
-        return cards[randCard]
+        lastChoice = random.randint(1,52)
+        cards.shuffle(lastChoice)
+        return cards.shuffledDeck[lastChoice]
